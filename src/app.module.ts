9aa0a07ec129d55c6423e576f22aedbe920a2ce2
Eduardo Souto dos Santos <bls.dudu@gmail.com>
2023-08-23 15:21:34 -0300

    build: add initial posts/medias/publications generated modules/controller/services structures

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { PostsModule } from './posts/posts.module';
+import { MediasModule } from './medias/medias.module';
+import { PublicationsModule } from './publications/publications.module';
+import { HealthModule } from './health/health.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [PostsModule, MediasModule, PublicationsModule, HealthModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
